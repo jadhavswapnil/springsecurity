57790ee6de664c3850c4cc21927671a4dbdf4265
Neosoft <Neosoft@PUNE-LAPTOP127>
2018-07-26 16:58:15 +0530

    This test project forlocal git

---
+++ src/main/java/com/neo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.neo.controller;
+
+import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/hello" , method=RequestMethod.GET)
+	public ModelAndView hello(){
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+	}
+	
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+}
